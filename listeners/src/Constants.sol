dea2529b617ae1c4ba5d56cb532886ae0d37e08c
Tal Vaizman <tal@comitylabs.com>
2025-07-21 16:04:53 +0300

    small listeners refactor

---
+++ listeners/src/Constants.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+address constant FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+address constant POSITION_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
--- listeners/test/UniswapV3PositionsListener.t.sol
+++ listeners/test/UniswapV3PositionsListener.t.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
-import {Listener} from "../src/Main.sol";
+import {UniswapV3PositionsListener} from "../src/UniswapV3PositionsListener.sol";
 import "sim-idx-sol/Simidx.sol";
 import {MockContexts} from "sim-idx-sol/test/MockContexts.sol";
 
-contract ListenerTest is Test {
-    Listener public listener;
+contract UniswapV3PositionsListenerTest is Test {
+    UniswapV3PositionsListener public listener;
 
     function setUp() public {
-        listener = new Listener();
+        listener = new UniswapV3PositionsListener();
         vm.recordLogs();
     }
 }
